79e54c3b473f14eb8bac42e35cbccc64622dde13
Marcos Santos <marcosestudante93@gmail.com>
2023-08-22 15:59:25 -0300

    alura

--- app/metodoSort.js
+++
@@ -1,7 +0,0 @@
-let btnOrdenarPorPreco = document.getElementById("btnOrdenarPorPreco")
-btnOrdenarPorPreco.addEventListener("click", ordenarLivrosPorPreco)
-
-function ordenarLivrosPorPreco() {
-   let livrosOrdenados =  livros.sort((a,b) => a.preco - b.preco)
-   exibirOsLivrosNaTela(livrosOrdenados)
-}
